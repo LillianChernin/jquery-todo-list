f90b30938adfd4c31446a42ae672811f5823b7b2
Lillian Chernin <l.smith1654@gmail.com>
2017-12-15 15:21:56 -0600

    add functionality to modify todos

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 
 const deleteButton = "<button class=" + "delete" + ">X</button>";
 const completeButton = "<button class=" + "complete" + ">Got'r done?</button>";
+const saveButton = "<button class=" + "save" + ">Save</button>";
 
 
-$('#addToDo').click((event) => {
-  let postText = $('input').val();
-  $('ol').prepend("<li>" + completeButton + " " + postText + " " + deleteButton + "</li>");
-  $('input').val("");
+
+const deleteToDo = () => {
   $('.delete').click((event) => {
     $(event.target).parent().remove();
   });
+}
+
+const completeToDo = () => {
   $('.complete').click((event) => {
     $(event.target).parent().css("text-decoration", "line-through");
     $(event.target).remove();
   });
+}
+
+const modifyToDo = () => {
+  $('a').dblclick((event) => {
+    let $currentText = $(event.target).text();
+    $(event.target).parent().append(saveButton);
+    $(event.target).replaceWith("<input type=" + "text" + " class=" + "updateText" + " value=" + $currentText + " >");
+    $('.save').click((event) => {
+      let $newText = $(event.target).parent().find('.updateText').val();
+      $(event.target).parent().find('.updateText').replaceWith("<a> " + $newText + " </a>");
+      $(event.target).remove();
+    })
+  })
+}
+
+
+
+$('#addToDo').click((event) => {
+  let postText = $('input').val();
+  $('ol').append("<li>" + completeButton + "<a> " + postText + " </a>" + deleteButton + "</li>");
+  $('input').val("");
+  deleteToDo();
+  completeToDo();
+  modifyToDo();
 })
