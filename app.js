aa32ad977bac02d5c90ddbb476b751a1772f8d93
Lillian Chernin <l.smith1654@gmail.com>
2017-12-15 12:19:01 -0600

    add functionality to complete and delete buttons

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 console.log("JS is working!");
 console.log($("h1").text());
 
-const deleteButton = "<button class=" + "delete" + ">Delete</button>";
-const completeButton = "<button class=" + "complete" + ">Completed?</button>";
+const deleteButton = "<button class=" + "delete" + ">X</button>";
+const completeButton = "<button class=" + "complete" + ">Complete</button>";
 
-$('#addToDo').click(() => {
+
+
+
+$('#addToDo').click((event) => {
   let postText = $('input').val();
   $('ol').prepend("<li>" + completeButton + " " + postText + " " + deleteButton + "</li>");
   $('input').val("");
-})
-
-$('.delete').click(() => {
-  $(this).parent().remove();
-  console.log("delete button was clicked!");
+  $('.delete').click((event) => {
+    $(event.target).parent().remove();
+  });
+  $('.complete').click((event) => {
+    $(event.target).parent().css("text-decoration", "line-through");
+    $(event.target).remove();
+  });
 })
